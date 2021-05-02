7a6ece1d1f72502bd9d084c5a4e6ac61b423c32d
ErastoRuuuuuush <e44liu@student.cs.uwaterloo.ca>
2021-05-02 02:40:09 -0400

    删除了所有东西

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS246-Constructor
-The final project for CS246 Fall 2020, bulit by Eric Liu and Trevor Chen.
--- res_info.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RES_INFO_H__
-#define __RES_INFO_H__
-
-enum class Info { None, Basement, House, Tower };
-
-#endif
